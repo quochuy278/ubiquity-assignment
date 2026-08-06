297b4a438c09a32508501c919dd723ee39c712ac
quochuy278 <quochuy270800@gmail.com>
2026-08-06 11:19:08 +0200

    refactor (server):  move app module into a dedicated directory

--- server/test/e2e/app.e2e-spec.ts
+++ server/test/e2e/app.e2e-spec.ts
@@ -2,7 +2,7 @@ import type { INestApplication } from '@nestjs/common';
 import { Test, type TestingModule } from '@nestjs/testing';
 import request from 'supertest';
 import type { App } from 'supertest/types';
-import { AppModule } from '../../src/app.module';
+import { AppModule } from '../../src/app/app.module';
 
 describe('AppController (e2e)', () => {
   let app: INestApplication<App>;
